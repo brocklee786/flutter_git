3c14fa7cd9bd776b7abc7adf14edf970bd335da4
furuyama_mac <kyabetsutarou333@gmail.com>
2024-10-07 16:19:17 +0900

    mac

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter',
       theme: ThemeData(
 
         // Notice that the counter didn't reset back to zero; the application
